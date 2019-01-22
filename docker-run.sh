e414e794a0c2df9edf5ef8c26c38d2aa9492323c
Anonym-Bruker <github@modalsli.com>
2019-01-22 15:01:03 +0100

    Updated env vars

--- docker-run.sh
+++ docker-run.sh
@@ -6,4 +6,4 @@ docker rm fdic-api
 echo '=====>docker build'
 docker build --no-cache -t fdic-api .
 echo '=====>docker run'
-docker run -d --restart always --name fdic-api --link couchdb -p 3000:3000 -e "NODE_ENV=local" fdic-api
+docker run -d --restart always --name fdic-api --network fdic-api -p 3000:3000 -e "NODE_ENV=local" fdic-api
